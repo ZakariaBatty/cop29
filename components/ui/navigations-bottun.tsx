2760f2c5b3230026133ab14004af762938097cb8
ZakariaBatty <zbatty1297@gmail.com>
2024-11-02 15:20:12 +0100

    add htaccess and github actions

--- components/ui/navigations-bottun.tsx
+++ components/ui/navigations-bottun.tsx
@@ -27,7 +27,7 @@ export default function NavigationButtons() {
       <Button
         variant="outline"
         size="icon"
-        className="fixed top-[38%] md:w-20 md:h-20 right-4 z-50 bg-black  "
+        className="fixed top-[42%] md:top-[45%] md:w-14 md:h-14 right-4 z-50 bg-black "
         onClick={() => router.back()}
       >
         <ArrowLeft className="h-9 w-9 text-white hover:text-black" />
@@ -35,7 +35,7 @@ export default function NavigationButtons() {
       <Button
         variant="outline"
         size="icon"
-        className="fixed bottom-[37%] right-4  md:w-20 md:h-20  z-50 bg-black "
+        className="fixed bottom-[48%] md:bottom-[37%] right-4  md:w-14 md:h-14  z-50 bg-black "
         onClick={() => router.push('/')}
       >
         <Home className="h-9 w-9 text-white hover:text-black" />
