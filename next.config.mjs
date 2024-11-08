ad82b4b2f95fa0fabebb4bfb6b9e2e7141342e5e
ZakariaBatty <zbatty1297@gmail.com>
2024-11-08 18:39:32 +0100

    up

--- next.config.mjs
+++ next.config.mjs
@@ -6,18 +6,7 @@ const nextConfig = {
   },
   basePath: '/cop29',
   assetPrefix: '/cop29/',
-  trailingSlash: true,
-  // Configure rewrites to handle direct URL access
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/cop29/:path*',
-          destination: '/:path*',
-        },
-      ],
-    }
-  },
+
 };
 
 export default nextConfig;
